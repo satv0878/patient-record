9272d99b5c6b96f5eec9dad9aeb649b30d06a0a4
Tobias Verleger <tsv@andever.de>
2021-10-20 17:10:13 +0200

    Added first container to the project

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ In the Virtual Environment you can run the functional test by
 ```bash 
 python functional_tests.py
 ``` 
+
+# Contribute 
+
+## Adding a django container
+After installing the django dependency you can create a container by executing 
+```bash 
+django-admin.py startproject patient_record .
+```
--- functional_tests.py
+++ functional_tests.py
@@ -10,5 +10,8 @@ header_text = browser.find_element_by_tag_name("h1").text
 unittest.assertIn("EMR", header_text)
 # A name Textbox, DOB, Address Textbox is available
 
-# When adding a name , address and clicking on add a patient will apeat in the list
+# When adding a name , address and clicking on add a patient will appear in the list
 # When adding a patient a unique id will be visible
+
+# close browser
+browser.quit()
