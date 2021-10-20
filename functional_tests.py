a7075529dbea652a1dad0eb569b021b6fcee7923
Tobias Verleger <tsv@andever.de>
2021-10-20 16:48:43 +0200

    initial commit

---
+++ functional_tests.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import unittest
+
+browser = webdriver.Firefox()
+
+# An all patients files are availbale on the webbroser
+browser.get("http://localhost:8000")
+
+header_text = browser.find_element_by_tag_name("h1").text
+unittest.assertIn("EMR", header_text)
+# A name Textbox, DOB, Address Textbox is available
+
+# When adding a name , address and clicking on add a patient will apeat in the list
+# When adding a patient a unique id will be visible
